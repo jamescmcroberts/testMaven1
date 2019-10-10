5bf9d4357466077d5866aad128178ded15f1d26b
ryan <rtheophall@wheelsup.com>
2019-10-10 16:10:19 -0400

    Uploading files

---
+++ src/test/java/MyFirstProject.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyFirstProject {
+
+    @BeforeTest
+    public void mainMethod() {
+        System.out.println("Main Method");
+    }
+
+    @Test
+    public void myFirstMethod() {
+        System.out.println("My First Method");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("After Test");
+    }
+}
